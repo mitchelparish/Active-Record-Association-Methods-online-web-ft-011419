70a369ff58684c40f6a796cde27546d92df1a02e
Mitchel Parish <mitchelparish@gmail.com>
2019-02-21 18:09:48 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,8 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved songz
-    end
+    self.songs.first.genre
+  end
 
   def song_count
     #return the number of songs associated with the artist
